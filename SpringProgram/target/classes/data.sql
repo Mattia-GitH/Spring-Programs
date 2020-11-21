00d62bf7f85fdc61423f3104bcfe6f40df5db7b8
Mattia <mattia.schiavo@mail.com>
2020-11-21 16:40:24 +0100

    Added dara.sql

---
+++ SpringProgram/target/classes/data.sql
@@ -0,0 +1,11 @@
+INSERT INTO PUBLIC.TBL_STUDENT (NAME, SURNAME, AGE) VALUES ( 'Mattia','Schiavo',18 ),
+                                                           ( 'Paolo','Bianchi',20 ),
+                                                           ( 'Luca','Rossi',30 ),
+                                                           ( 'Paolo','Verdi',25 ),
+                                                           ( 'Giuseppe','Brown',54 );
+
+INSERT INTO PUBLIC.TBL_BOOK (TITLE, AUTHOR, ISBN) VALUES ( 'Il buio tra la siepe', 'Paolo Bianchi','IUD5463874' ),
+                                                         ('Il fu Mattia Pascal', 'Luca Rossi','GF298412HJ'),
+                                                         ('Il giovane holden','Luca Rossi','85DVR21SE2'),
+                                                         ('Orgoglio e Pregiudizzio','Giorgio Verdi','H5S1VC2894'),
+                                                         ('La notte','Paolo Bianchi','IUH521W45Z');
