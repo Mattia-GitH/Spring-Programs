e011240525ed244bfebaa48564df38961875b26f
Mattia <mattia.schiavo@mail.com>
2020-12-09 17:28:20 +0100

    CRUD service layer tested

---
+++ SpringProgram/src/test/java/com/SpringH2DB/SpringH2/service/BookServiceTest.java
@@ -0,0 +1,71 @@
+package com.SpringH2DB.SpringH2.service;
+
+
+import com.SpringH2DB.SpringH2.model.BookModel;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+@SpringBootTest
+public class BookServiceTest {
+
+    @Autowired
+    private BookService bookService;
+
+    @Test
+    void testing_createBook(){
+        BookModel toCreate = new BookModel();
+        toCreate.setAuthor("Author");
+        toCreate.setTitle("Title");
+
+        BookModel created = bookService.createBook(toCreate);
+        toCreate.setId(created.getId());
+
+        Assertions.assertEquals(created,toCreate);
+    }
+
+    @Test
+    void testing_update(){
+        BookModel oldBook = new BookModel();
+        oldBook.setAuthor("Author");
+        bookService.createBook(oldBook);
+
+        BookModel update = new BookModel();
+        update.setId(1);
+        update.setAuthor("Author Updated");
+        BookModel updated = bookService.updateBook(update);
+
+        Assertions.assertEquals(update.getAuthor(), updated.getAuthor());
+    }
+
+    @Test
+    void testing_listBook(){
+        BookModel book1 = new BookModel();
+        BookModel book2 = new BookModel();
+
+        bookService.createBook(book1);
+        bookService.createBook(book2);
+
+        List<BookModel> bookModels = bookService.listBook();
+
+        Assertions.assertEquals(2,bookModels.size());
+    }
+
+    @Test
+    void testing_delete(){
+        BookModel bookModel = new BookModel();
+        bookModel.setAuthor("Author");
+        bookModel.setTitle("Title");
+        bookService.createBook(bookModel);
+
+        bookService.deleteBook(1L);
+        List<BookModel> bookModels = bookService.listBook();
+
+        Assertions.assertEquals(0,bookModels.size());
+    }
+
+
+}
---
+++ SpringProgram/src/test/java/com/SpringH2DB/SpringH2/service/StudentServiceTest.java
@@ -0,0 +1,69 @@
+package com.SpringH2DB.SpringH2.service;
+
+import com.SpringH2DB.SpringH2.model.StudentModel;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+@SpringBootTest
+public class StudentServiceTest {
+
+    @Autowired
+    private StudentService studentService;
+
+    @Test
+    void testing_createStudent(){
+        StudentModel toCreate = new StudentModel();
+        toCreate.setName("Name");
+        toCreate.setSurname("Surname");
+        toCreate.setAge(18);
+
+        StudentModel created = studentService.createStudent(toCreate);
+        toCreate.setId(created.getId());
+
+        Assertions.assertEquals(toCreate, created);
+    }
+
+    @Test
+    void testing_update(){
+        StudentModel toUpdate = new StudentModel();
+        toUpdate.setName("Name");
+        studentService.createStudent(toUpdate);
+
+        StudentModel update = new StudentModel();
+        update.setId(1);
+        update.setName("Name Updated");
+        StudentModel updated = studentService.updateStudent(update);
+
+        Assertions.assertEquals(update.getName(), updated.getName());
+    }
+
+    @Test
+    void testing_listStudents(){
+        StudentModel student1 = new StudentModel();
+        StudentModel student2 = new StudentModel();
+
+        studentService.createStudent(student1);
+        studentService.createStudent(student2);
+
+        List<StudentModel> studentModelList = studentService.listStudents();
+
+        Assertions.assertEquals(2,studentModelList.size());
+    }
+
+    @Test
+    void testing_delete(){
+        StudentModel studentModel = new StudentModel();
+        studentModel.setName("Name");
+        studentModel.setSurname("Surname");
+        studentService.createStudent(studentModel);
+
+        studentService.deleteStudent(1L);
+        List<StudentModel> bookModels = studentService.listStudents();
+
+        Assertions.assertEquals(0,bookModels.size());
+    }
+}
